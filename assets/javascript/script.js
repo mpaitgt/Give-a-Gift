af4fb484acb444f54b07d7fbf8df2b83dd7eb4c9
Matt Pignatore <mattpigs@gmail.com>
2019-09-28 19:51:07 -0400

    added api call with button event listeners

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,43 +1,59 @@
 var btnBox = $('#btn-box');
+var gifBox = $('#gif-box');
 var topics = ['tv shows', 'movies', 'bands', 'books', 'food'];
 var search = $('#search-btn');
 
 window.onload = function() {
+    updateButtons();
 
-    search.on('click', function() {
+    search.on('click', function() {     // search button - something about this is fucking everything up
         var searchItem = $('#search-input').val();
         topics.push(searchItem);
         updateButtons();
     });
 
-    currentBtn.on('click', '.btnOptions', function() {
-        var APIkey = 'BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9';
-        var queryURL = 'https://api.giphy.com/v1/gifs/trending?api_key=' + APIkey;
+    $(document).on('click', '.gif-btn', function() {      //gif-created buttons
+        console.log('hello');
+        var APIkey = 'MVCunlgGr8xkVSFtMCzMpFYtpSwG5C17';
+        var btnTopic = $(this).text().trim();
+        console.log(btnTopic);
+        var queryURL = 'https://api.giphy.com/v1/gifs/trending?api_key=' + APIkey + '&q=' + btnTopic;
 
         $.ajax({            // ajax call
             url: queryURL,
             method: 'GET'
         }).then(function(response) {
             console.log(response);
+            for (var i = 0; i < 5; i++) {       // displays the gif on the page
+                
+                var newDiv = $('<div>');
+                newDiv.addClass('gif-holder');
+                var gifImage = response.data[i].images.fixed_width.url;
+                var newGif = $('<img>');
+                newGif.attr('src', gifImage);
+                newDiv.append(newGif);
+                gifBox.prepend(newDiv);
+            }
         });
     })
 }
 
-function updateButtons() {
+
+function updateButtons() {      // displays the button on the page
     btnBox.empty();
     for (var i = 0; i < topics.length; i++) {
-        var newBtn = $('<button>').addClass('col-12');
-        //display the current array item text within the button
+        var newBtn = $('<button>');
+        newBtn.addClass('gif-btn');
+        newBtn.attr('data-name');
         newBtn.text(topics[i]);
-        //append the button to the btnBox
         btnBox.append(newBtn);
+
     }
 }
 
 
 
 // called functions
-updateButtons();
 
 
 // add a button click function 
