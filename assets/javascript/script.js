c66bb3ebc686baea1e4cf5b6b4f4acd68fc8e839
Matt Pignatore <mattpigs@gmail.com>
2019-09-28 20:10:32 -0400

    added keyword api parameter

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,53 +6,53 @@ var search = $('#search-btn');
 window.onload = function() {
     updateButtons();
 
-    search.on('click', function() {     // search button - something about this is fucking everything up
+    search.on('click', function() {     // search button
         var searchItem = $('#search-input').val();
         topics.push(searchItem);
         updateButtons();
     });
 
-    $(document).on('click', '.gif-btn', function() {      //gif-created buttons
-        console.log('hello');
-        var APIkey = 'MVCunlgGr8xkVSFtMCzMpFYtpSwG5C17';
-        var btnTopic = $(this).text().trim();
-        console.log(btnTopic);
-        var queryURL = 'https://api.giphy.com/v1/gifs/trending?api_key=' + APIkey + '&q=' + btnTopic;
-
-        $.ajax({            // ajax call
-            url: queryURL,
-            method: 'GET'
-        }).then(function(response) {
-            console.log(response);
-            for (var i = 0; i < 5; i++) {       // displays the gif on the page
-                
-                var newDiv = $('<div>');
-                newDiv.addClass('gif-holder');
-                var gifImage = response.data[i].images.fixed_width.url;
-                var newGif = $('<img>');
-                newGif.attr('src', gifImage);
-                newDiv.append(newGif);
-                gifBox.prepend(newDiv);
-            }
-        });
-    })
+    $(document).on('click', '.gif-btn', displayGIF);      // gif-created buttons
 }
 
 
 function updateButtons() {      // displays the button on the page
     btnBox.empty();
+
     for (var i = 0; i < topics.length; i++) {
         var newBtn = $('<button>');
         newBtn.addClass('gif-btn');
-        newBtn.attr('data-name');
+        newBtn.attr('data-name', topics[i]);
         newBtn.text(topics[i]);
         btnBox.append(newBtn);
-
     }
 }
 
 
+function displayGIF() {
+    var btnTopic = $(this).attr('data-name');
+    var APIkey = 'MVCunlgGr8xkVSFtMCzMpFYtpSwG5C17';
+    var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=' + APIkey + '&q=' + btnTopic;
 
+    $.ajax({     
+               // ajax call
+        url: queryURL,
+        method: 'GET'
+    }).then(function(response) {
+
+        console.log(response);
+        for (var i = 0; i < 5; i++) {       // displays the gif on the page
+
+            var newDiv = $('<div>');
+            newDiv.addClass('gif-holder');
+            var gifImage = response.data[i].images.fixed_width.url;
+            var newGif = $('<img>');
+            newGif.attr('src', gifImage);
+            newDiv.append(newGif);
+            gifBox.prepend(newDiv);
+        }
+    });
+}
 // called functions
 
 
