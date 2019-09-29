266fcd2da443896e22ca13f50f51ea579607c77d
Matt Pignatore <mattpigs@gmail.com>
2019-09-28 22:34:32 -0400

    fixed the duplicate button bug

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,15 +1,20 @@
 var btnBox = $('#btn-box');
 var gifBox = $('#gif-box');
-var topics = ['tv shows', 'movies', 'bands', 'books', 'food'];
+var topics = ['arrested development', 'the office', 'lost', 'the wire', 'the good place', 'twin peaks', 'the leftovers', 'parks and recreation', 'game of thrones'];
 var search = $('#search-btn');
+
+
 
 window.onload = function() {
     updateButtons();
 
     search.on('click', function() {     // search button
         var searchItem = $('#search-input').val();
-        topics.push(searchItem);
-        updateButtons();
+
+        if (!topics.includes(searchItem)) {
+            topics.push(searchItem);
+            updateButtons();
+        }   
     });
 
     $(document).on('click', '.gif-btn', displayGIF);      // gif-created buttons
@@ -28,32 +33,30 @@ function updateButtons() {      // displays the button on the page
     }
 }
 
-
 function displayGIF() {
     var btnTopic = $(this).attr('data-name');
     var APIkey = 'MVCunlgGr8xkVSFtMCzMpFYtpSwG5C17';
-    var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=' + APIkey + '&q=' + btnTopic;
+    var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=' + APIkey + '&q=' + btnTopic +'&limit=20';
 
-    $.ajax({     
-               // ajax call
+    $.ajax({     // ajax call
         url: queryURL,
         method: 'GET'
     }).then(function(response) {
 
         console.log(response);
-        for (var i = 0; i < 5; i++) {       // displays the gif on the page
+        for (var i = 0; i < 10; i++) {       // displays the gif on the page
 
             var newDiv = $('<div>');
             newDiv.addClass('gif-holder');
-            var gifImage = response.data[i].images.fixed_width.url;
+            var gifImage = response.data[i].images.fixed_height.url;
             var newGif = $('<img>');
             newGif.attr('src', gifImage);
-            newDiv.append(newGif);
+
+            var newP = $('<p>');
+            newP.text(response.data[i].rating);
+
+            newDiv.append(newGif, newP);
             gifBox.prepend(newDiv);
         }
     });
 }
-// called functions
-
-
-// add a button click function 
